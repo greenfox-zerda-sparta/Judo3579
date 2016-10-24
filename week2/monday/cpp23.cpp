85a08206580b903777fc17b38d2db61389db3746
Judo3579 <istvan.judit5@gmail.com>
2016-10-24 17:47:06 +0200

    first excercises

---
+++ week2/monday/cpp23.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+		int ad = 6;
+		int i = 0;
+		while (i <= ad){
+
+			cout << i << endl;
+
+			++i;
+
+
+		}
+	return 0;
+}
