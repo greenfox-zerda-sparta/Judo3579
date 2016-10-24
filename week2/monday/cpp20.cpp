0dce135b3d891e6e5e6b8e771350befdc2f48b64
Judo3579 <istvan.judit5@gmail.com>
2016-10-24 17:12:32 +0200

    first excercises

---
+++ week2/monday/cpp20.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+		string x = "cheese";
+		int y = x.length();
+
+		 if(y % 2 == 0){
+
+			cout<< "True" << endl;
+
+		 }else{
+			 cout<< "short" << endl;
+
+		}
+	return 0;
+}
