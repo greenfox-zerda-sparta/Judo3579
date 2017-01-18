641e25d02bc3cb5039b2979b53c29e844dfca679
Judo3579 <istvan.judit5@gmail.com>
2017-01-18 15:19:22 +0100

    I can send email

---
+++ project/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+using System.IO;
+using MimeKit.Utils;
+
+namespace TestClient
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("email@gmail.com"));
+            message.To.Add(new MailboxAddress("email@gmail.com"));
+            message.Subject = "How you doin'?";
+
+            var builder = new BodyBuilder();
+
+            // Set the plain-text version of the message text
+            builder.TextBody = @"Hey XY,
+
+What are you up to this weekend?
+
+-- J
+";
+
+            // In order to reference selfie.jpg from the html text, we'll need to add it
+            // to builder.LinkedResources and then use its Content-Id value in the img src.
+            var image = builder.LinkedResources.Add(@"path");
+            image.ContentId = MimeUtils.GenerateMessageId();
+
+            // Set the html version of the message text
+            builder.HtmlBody = string.Format(@"<p>Hey XY,<br>
+<p>What are you up to this weekend?
+<p>-- J<br>
+<center><img src=""cid:{0}""></center>", image.ContentId);
+
+            // We may also want to attach a calendar event for Monica's party...
+            builder.Attachments.Add(@"path");
+
+            // Now we just need to set the message body and we're done
+            message.Body = builder.ToMessageBody();
+
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("email", "password");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
