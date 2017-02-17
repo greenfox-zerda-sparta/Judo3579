a7e71ff523b05f6838ce16a567ba77ecf1ff4743
Judo3579 <istvan.judit5@gmail.com>
2017-02-17 09:51:54 +0100

    beugro is done

---
+++ beugro/beugro.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : beugro.cpp
+// Author      : haha
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+  int arr[6] = {1, 3, 8, 6, 7, 4};
+  int temp = 0;
+
+  for (int i=0; i<6; i++) {
+    if (arr[i] % 2 && arr[i] > temp)
+   	temp = arr[i-1];
+    }
+
+  cout << "The biggest number is: " << temp << endl;
+  return 0;
+}
