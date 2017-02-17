2d0db413ba3fe52ae5eb511fe7cfcff0c9882dec
Judo3579 <istvan.judit5@gmail.com>
2017-02-17 10:17:54 +0100

    misunderstood the example, function added

--- beugro/beugro.cpp
+++ beugro/beugro.cpp
@@ -10,18 +10,22 @@
 
 using namespace std;
 
+int second_biggest_number (int arr[]) {
+  int temp;
+  for (int i=0; i<6; i++) {
+    if (arr[i] % 2 && arr[i] > temp) {
+	  temp = arr[i-1];
+    }
+  }
+  return temp;
+}
+
 
 int main() {
 
   int arr[6] = {1, 3, 8, 6, 7, 4};
-  int temp = 0;
+  int x = second_biggest_number(arr);
 
-  for (int i=0; i<6; i++) {
-    if (arr[i] % 2 && arr[i] > temp) {
-   	temp = arr[i-1];
-    }
-  }
-
-  cout << "The biggest number is: " << temp << endl;
+  cout << "The biggest number is: " << x << endl;
   return 0;
 }
